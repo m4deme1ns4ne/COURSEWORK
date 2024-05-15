e019d03d8287be8542096dc88d312666a3001357
Alexander <alexandervolzhanin2004@gmail.com>
2024-05-15 08:59:26 +0300

    xzc

--- Курсовая работа. Прыжок с парашютом./Задание 3/main.py
+++ Курсовая работа. Прыжок с парашютом./Задание 3/main.py
@@ -1,46 +1,61 @@
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
-# Исходные параметры модели
-g = 9.8
-m0 = 70
-mp = 15
-m = m0+mp
-r = 0.3
-S0 = np.pi*r**2
-R = 5
-S1 = np.pi*R**2
-ro = 1.3
-Cx0 = 0.5
-Cx1 = 1.28
-k0 = Cx0*ro*S0/2
-k1 = Cx1*ro*S1/2
-H = 46
-tr = 3
-v0 = 0
-# Расчёт математической (теоретической) модели
-T = 500
-N = 20000
-dt = T/N
-y = np.zeros(N)
-v = np.zeros(N)
-t = np.zeros(N)
-y[0] = H
-v[0] = v0
-t[0] = 0
-k = k0
-for i in range(N-1):
-    if t[i]>=tr:
-        k = k1
-    y[i+1] = y[i]+v[i]*dt
-    v[i+1] = v[i]-(g+k/m*abs(v[i])*v[i])*dt
-    if y[i+1]<0:
-        break
-    t[i+1] = t[i]+dt
-# Вывод полученного графика
-plt.title("v = v(y)")
-plt.xlabel('y, м', fontsize=14)
-plt.ylabel('v, м/с', fontsize=14)
-plt.plot(y[:i], v[:i])
-plt.grid()
-plt.tight_layout()
-plt.show()
+
+
+class RocketSimulation:
+    def __init__(self):
+        # Исходные параметры модели
+        self.g = 9.8
+        self.m0 = 70
+        self.mp = 15
+        self.m = self.m0 + self.mp
+        self.r = 0.3
+        self.S0 = np.pi * self.r ** 2
+        self.R = 5
+        self.S1 = np.pi * self.R ** 2
+        self.ro = 1.3
+        self.Cx0 = 0.5
+        self.Cx1 = 1.28
+        self.k0 = self.Cx0 * self.ro * self.S0 / 2
+        self.k1 = self.Cx1 * self.ro * self.S1 / 2
+        self.H = 46
+        self.tr = 3
+        self.v0 = 0
+
+        # Расчёт математической (теоретической) модели
+        self.T = 500
+        self.N = 20000
+        self.dt = self.T / self.N
+        self.y = np.zeros(self.N)
+        self.v = np.zeros(self.N)
+        self.t = np.zeros(self.N)
+        self.y[0] = self.H
+        self.v[0] = self.v0
+        self.t[0] = 0
+
+    def calculate_trajectory(self):
+        k = self.k0
+        for i in range(self.N - 1):
+            if self.t[i] >= self.tr:
+                k = self.k1
+            self.y[i + 1] = self.y[i] + self.v[i] * self.dt
+            self.v[i + 1] = self.v[i] - (self.g + k / self.m * abs(self.v[i]) * self.v[i]) * self.dt
+            if self.y[i + 1] < 0:
+                break
+            self.t[i + 1] = self.t[i] + self.dt
+
+    def plot_velocity_vs_height(self):
+        i = np.argmax(self.y < 0)
+        
+        plt.title("v = v(y)")
+        plt.xlabel('y, м', fontsize=14)
+        plt.ylabel('v, м/с', fontsize=14)
+        plt.plot(self.y[:i], self.v[:i])
+        plt.grid()
+        plt.tight_layout()
+        plt.show()
+
+if __name__ == "__main__":
+    rocket = RocketSimulation()
+    rocket.calculate_trajectory()
+    rocket.plot_velocity_vs_height()
